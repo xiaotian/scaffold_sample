a7c5c60e11904d4393dd574fd008d1d737ffac5f
Xiaotian Guo <guoxiaotian@gmail.com>
2012-01-24 22:35:39 -0800

    ran migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
 end
+# == Schema Information
+# Schema version: 20120125063029
+#
+# Table name: posts
+#
+#  id         :integer         not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20120125063029
+#
+# Table name: posts
+#
+#  id         :integer         not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Post do
